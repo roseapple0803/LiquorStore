3f487635799556c194f58d44f836d775ef44bcae
RoseApple <jyueh.ca@gmail.com>
2016-01-24 10:43:07 -0500

    commit ui.R only

---
+++ ui.R
@@ -0,0 +1,28 @@
+library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel("Welcome to BC Liquor Store",windowTitle = "Liquor Price"),
+  
+  sidebarLayout(
+    sidebarPanel(  
+      sliderInput("price", "Choose your price range:", min=1, max=100, c(5,40), pre="$"), 
+      br(),
+      uiOutput("productUI"),
+      uiOutput("countryUI"),
+      
+      tableOutput("subtypeList")
+
+    ),
+
+    mainPanel(
+      h4("Price Range:", textOutput("captionPrice", container = span), style="color:lightblue"),
+      h4("Product Chosen: ", textOutput("captionProduct", container = span), style="color:lightblue"),
+      br,
+      #plotOutput("liquorplot")
+      tableOutput("summary")
+
+ 
+    )
+  )
+  
+))
