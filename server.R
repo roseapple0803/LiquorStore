ee8262cf760697f2a60f0515b311e240ac26201e
RoseApple <jyueh.ca@gmail.com>
2016-01-24 11:31:00 -0500

    copy from test1 directory again

--- server.R
+++ server.R
@@ -1,14 +1,20 @@
 library(shiny)
-library(ggplot2)
 library(plyr)
 library(dplyr)
+library(ggplot2)
 library(DT)
+
 
 mydf <- read.csv("bcl-data.csv", stringsAsFactors=FALSE)
 
-hasData <- function(x)
+
+isAcceptable <- function(x)
 {
-  if (nrow(x) == 0){
+  if (is.null(x)){
+    
+    return (FALSE)
+  }
+  else if (nrow(x) == 0){
     
     return (FALSE)
   }    
@@ -18,62 +24,21 @@ hasData <- function(x)
 
 
 
-shinyServer(function(input, output, session) {
+################################################################## 
+
+
+shinyServer(function(input, output) {
   
-  output$captionPrice <- renderText({
-    paste("Price range: ", input$price[1], "to ", input$price[2], " dollars")
-  })
-  
-  output$captionProduct <- renderText({
-    paste("Product: ", input$type, " from ", input$countryInput)
-  })
-  
-  
-  
-  bigdf <- reactive({
-    mydf %>% filter(Price >= input$price[1], Price <= input$price[2], 
-                    Type == input$type)
-  })
-  
-  
-  theDF <- reactive({    
-    dataset <- bigdf()
-    
-    if (nrow(dataset) == 0){
-      return ()
-    }
-    
-    dataset %>%
-      filter(
-        #Subtype == input$subtype,
-        
-        Country == input$countryInput
-      )
+  currDataset <- reactive({
+    mydf %>% filter(Price >= input$price[1], Price <= input$price[2])
   })
   
   
   
-  dfTally <- reactive({    
-    ddply(theDF(), c("Country", "Type", "Subtype"), summarise, 
-          AVG_PRICE=mean(Price, na.rm=TRUE), MIN_PRICE=min(Price, na.rm=TRUE), 
-          MAX_PRICE=max(Price, na.rm=TRUE), AVG_ALCOHOL_CONTENT=mean(Alcohol_Content), TOTAL=length(Price))
-    
+  bigDF <- reactive({
+    currDataset() %>% filter(Type == input$type, Country == input$countryInput)
   })
   
-  
-  ###########################################################  
-  
-  dfSubtype <- reactive({
-    #     if (is.null(theDF())){
-    #       return (NULL)  
-    #     }
-    
-    dataset <- theDF() %>% filter(Type == input$type) %>% select(Subtype) %>% distinct()
-    
-    
-  })
-  
-  ###########################################################  
   
   
   output$captionPrice <- renderText({
@@ -81,7 +46,7 @@ shinyServer(function(input, output, session) {
   })
   
   output$captionProduct <- renderText({ 
-    if (hasData(currDataset())){
+    if (isAcceptable(currDataset())){
       paste(input$type, " from ", input$countryInput)
     }
     else
@@ -89,120 +54,80 @@ shinyServer(function(input, output, session) {
   })
   
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
   output$productUI <- renderUI({
-    radioButtons("type", "Product type:", unique(mydf$Type), selected = "WINE")  
+    if (isAcceptable(currDataset())){
+      df <- currDataset()
+      radioButtons("type", "Product type:", unique(df$Type), selected = unique(df$Type)[1])
+    }
+    
+    else
+      (NULL)
   })
   
   
   output$countryUI <- renderUI({
-    if (is.null(bigdf())){
-      return (NULL)  
+    if (isAcceptable(currDataset())){
+      
+      if (!is.null(input$type)){
+        thedf <- currDataset() %>% filter(Type == input$type)
+        selectInput("countryInput", "Country of Origin", sort(unique(thedf$Country)), 
+                    selected = thedf$Country[1]) 
+      }
     }
     
-    
-    dataset <- bigdf()
-    if (nrow(dataset) == 0){
-      return ()
-    }
-    
-    selectInput("countryInput", "Country of Origin",
-                sort(unique(dataset$Country)),
-                selected = dataset$Country[1])
+    else
+      (NULL)
   })
   
-  
-  output$subtypeUI <- renderUI({
-    # radioButtons("subtype", "Available in:", unique(dfSubtype()$Subtype), selected = dfSubtype()$Subtype[1])
-  })
-  
-  ########################################################### 
-  #  
-  #  output$subproductUI <- renderUI({
-  #    radioButtons("subtype", "Sub Product type:", unique(mydf$subType), selected = "Red WINE")      
-  #  })
-  #
   
   
   
   output$subtypeList <- renderTable({
-    dataset <- dfSubtype()
-    names(dataset) <- c("Available in")
-    dataset
-  })
-  
-  
-  
-  
-  output$liquorplot <- renderPlot({
-    if (is.null(theDF())) {
-      print("theDF is not ready")
-      return()
+    if (isAcceptable(currDataset())){
+      thedf <- currDataset() %>% filter(Type == input$type, Country == input$countryInput) %>% select(Subtype) %>% distinct()
+      names(thedf) <- c("Available in")
+      
+      if (isAcceptable(thedf)){
+        thedf 
+      }
     }
     
-    
-    ggplot(theDF(), aes(Alcohol_Content)) +
-      geom_histogram(colour="grey", fill="lightblue", bins=25) 
-    
-  })
-  
-  
-  #output$results <- renderTable({
-  output$results <- DT::renderDataTable({
-    dataset <- theDF()  
-    dataset[,c("Subtype", "Name", "Alcohol_Content", "Price", "Sweetness")]
+    else
+      (NULL)
   })
   
   
   
+  ######################################################
+  
+  output$liquorplot <- renderPlot({
+    if (isAcceptable(currDataset())){
+      ggplot(bigDF(), aes(Alcohol_Content)) +
+        geom_histogram(fill="lightblue", colour ="slateblue4", bins=25) 
+    }
+    
+  })
   
   
-  ###########################################################  
+  
+  
   output$summary <- renderTable({
-    dataset <- dfTally()
-    dataset[,c("Subtype", "AVG_PRICE", "MIN_PRICE", "MAX_PRICE", "AVG_ALCOHOL_CONTENT", "TOTAL")]
+    if (isAcceptable(currDataset())){
+      thedf <- ddply(bigDF(), c("Country", "Type", "Subtype"), summarise, 
+                     AVG_PRICE=mean(Price, na.rm=TRUE), MIN_PRICE=min(Price, na.rm=TRUE), 
+                     MAX_PRICE=max(Price, na.rm=TRUE), AVG_ALCOHOL_CONTENT=mean(Alcohol_Content), TOTAL_LABELS=length(Price))
+      
+      thedf %>% select(Subtype, AVG_PRICE, MIN_PRICE, MAX_PRICE, AVG_ALCOHOL_CONTENT, TOTAL_LABELS)
+    }
   })
   
   
-  ###########################################################  
   
-  observe({   
-    print(input$price)
+  output$results <- DT::renderDataTable({
+    if (isAcceptable(currDataset())){
+      thedf <- bigDF()
+      thedf[,c("Subtype", "Name", "Alcohol_Content", "Price", "Sweetness")]
+    }
   })
-  ########################################################### 
   
 })
-
-
-# selectInput("countryInput", "Country of Origin",
-#             sort(unique(mydf$Country)),
-#             selected = "CANDA")
-
-
-# theDF <- reactive({
-#       if (is.null(input$countryInput)){
-#         return (NULL)
-#       }
-#  
-#   mydf %>%
-#     filter(Price >= input$price[1],
-#            Price <= input$price[2],
-#            Type == input$type,
-#            
-#            #Subtype == input$subtype,
-#            
-#            Country == input$countryInput
-#     )
-# })
--- ui.R
+++ ui.R
@@ -1,28 +1,45 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
 library(shiny)
 
 shinyUI(fluidPage(
   titlePanel("Welcome to BC Liquor Store",windowTitle = "Liquor Price"),
   
+  
+  # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(  
-      sliderInput("price", "Choose your price range:", min=1, max=100, c(5,40), pre="$"), 
+      sliderInput("price", "Choose your price range:", min=1, max=100, c(5,40), pre="$"),
+      
+      
       br(),
       uiOutput("productUI"),
       uiOutput("countryUI"),
       
-      tableOutput("subtypeList")
-
-    ),
-
-    mainPanel(
-      textOutput("captionPrice"),
-      textOutput("captionProduct"),
+      tableOutput("subtypeList"),
       
+      uiOutput("numUI"),
+      uiOutput("selectUI")
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
       h4("Price Range:", textOutput("captionPrice", container = span), style="color:lightblue"),
-      h4("Product Chosen: ", textOutput("captionProduct", container = span), style="color:lightblue"),
-      br
-     
- 
+      h4("Product: ", textOutput("captionProduct", container = span), style="color:lightblue"),
+      br(),
+      
+      
+      tabsetPanel(type = "tabs", 
+                  tabPanel("Plot", plotOutput("liquorplot")), 
+                  tabPanel("Tally", tableOutput("summary")), 
+                  tabPanel("List", DT::dataTableOutput("results"))
+      )
+      
     )
   )
   
