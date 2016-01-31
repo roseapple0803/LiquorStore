d81654c2d7cb5758851806d169049e950cb1a37b
RoseApple <jyueh.ca@gmail.com>
2016-01-31 18:45:20 -0500

    final commit

--- server.R
+++ server.R
@@ -125,50 +125,50 @@ shinyServer(function(input, output) {
       (NULL)
   })
   
-  
-
-  
-  output$liquorplot <- renderPlot({
-    if (isAcceptable(currDataset())){
-      
-      thedf <- bigDF()
-      if (!is.null(thedf))
-      { 
-        # remove all the rows with NA Alcohol_Content 
-        thedf <- thedf[!is.na(thedf$Alcohol_Content),]
-        
-        thestart <- min(thedf$Alcohol_Content)
-        theend <- max(thedf$Alcohol_Content)
-        thestep <- ifelse((theend - thestart) >= 2, 2, 0.1)
-         
-        if (!is.null(thedf$Alcohol_Content)){
-          avgMsg <- paste("Average alcohol content within your search criteria is ", round(mean(thedf$Alcohol_Content),2))
-        
-          ggplot(thedf, aes(Alcohol_Content)) +
-            geom_histogram(fill="lightblue", colour ="slateblue4", bins=25) + 
  
-            ggtitle("Histogram of Alcohol Content") + ylab("Count") +
-            theme(plot.title=element_text(size=rel(1.5),face="bold")) + 
-        
-            scale_x_continuous(breaks=seq(thestart, theend, thestep)) +
-           
-          
-            theme(axis.text=element_text(size=12),
-                  axis.title=element_text(size=14,face="bold")) +
-            geom_vline(xintercept= mean(thedf$Alcohol_Content), col="salmon", size=1) + 
-            annotate("text", x=-Inf, y=Inf, label=avgMsg, hjust=-.2, vjust=3, label=avgMsg)
-        }
 
-        else{
-          print(thedf$Alcohol_Content)        
-          return()
-        }
-
-      }
-    }
+output$liquorplot <- renderPlot({
+  if (isAcceptable(currDataset())){
     
-  })
+    thedf <- bigDF()
+    
+    # be aware of empty dataset
+    if (isAcceptable(thedf))
+    { 
+      # remove all the rows with NA Alcohol_Content 
+      thedf <- thedf[!is.na(thedf$Alcohol_Content),]
+      
+      thestart <- min(thedf$Alcohol_Content)
+      theend <- max(thedf$Alcohol_Content)
+      thestep <- ifelse((theend - thestart) >= 2, 2, 0.1)
+      
+      if (!is.null(thedf$Alcohol_Content)){
+        avgMsg <- paste("Average alcohol content within your search criteria is ", round(mean(thedf$Alcohol_Content),2))
+        
+        ggplot(thedf, aes(Alcohol_Content))  +
+          geom_histogram(fill="lightblue", colour ="slateblue4", binwidth=0.5) + 
+          ggtitle("Histogram of Alcohol Content") + ylab("Count") +
+          theme(plot.title=element_text(size=rel(1.5),face="bold")) +
+          
+          #scale_x_continuous(breaks=seq(thestart, theend, thestep)) +
+          
+          theme(axis.text=element_text(size=12),
+          axis.title=element_text(size=14,face="bold")) +
+          geom_vline(xintercept= mean(thedf$Alcohol_Content), col="salmon", size=1) + 
+          annotate("text", x=-Inf, y=Inf, label=avgMsg, hjust=-.2, vjust=3, label=avgMsg)
+      }
+      
+      else{
+        print(thedf$Alcohol_Content)        
+        return()
+      }
+      
+    }
+  }
   
+})
+
+
 
  
   
@@ -196,7 +196,8 @@ shinyServer(function(input, output) {
         
           # make sure $AVG_PRICE is there
           if (!is.null(df$AVG_PRICE)){
-            df %>% select(Subtype, AVG_PRICE, MIN_PRICE, MAX_PRICE, AVG_ALCOHOL_CONTENT, TOTAL_LABELS)
+            df %>% select(Subtype, AVG_PRICE, MIN_PRICE, MAX_PRICE, AVG_ALCOHOL_CONTENT, TOTAL_LABELS) %>% 
+              arrange(TOTAL_LABELS)
           }
           else{
             #print(df$AVG_PRICE)
@@ -219,12 +220,14 @@ shinyServer(function(input, output) {
                     AVG_PRICE=mean(Price, na.rm=TRUE), MIN_PRICE=min(Price, na.rm=TRUE), 
                     MAX_PRICE=max(Price, na.rm=TRUE), AVG_ALCOHOL_CONTENT=mean(Alcohol_Content), TOTAL_LABELS=length(Price))
         
+        
         # make sure $AVG_PRICE is there
         if (!is.null(df$AVG_PRICE)){
           filtered <- df %>% select(Subtype, AVG_PRICE, MIN_PRICE, MAX_PRICE, AVG_ALCOHOL_CONTENT, TOTAL_LABELS)
           
           ggplot(data=filtered, aes(x=reorder(Subtype, TOTAL_LABELS), y=TOTAL_LABELS)) + 
-            geom_bar(stat = "identity", colour="red") + coord_flip()
+            geom_bar(stat = "identity", colour="slateblue4", fill="lightblue", width=.7) + coord_flip() + 
+            xlab("Available Subtype") + ylab("Total Labels")
         }
         else{
           #print(df$AVG_PRICE)
@@ -243,22 +246,18 @@ shinyServer(function(input, output) {
 #                  "Move the Number of bins slider and notice how the renderPlot expression is automatically 
 #                   re-evaluated when its dependant, input$bins,changes,causing a histogram with 
 #                  a new number of bins to be rendered.", "Hello", "World", sep="<br/><br/>"))
+
       
-      
-#       list("This samll ",  span("Shiny application", style='color:salmon'), "demonstrates Shiny's automatic UI updates.", br(),br(), 
-#            "Move the Number of bins slider and notice how the renderPlot expression is automatically 
-#                   re-evaluated when its dependant, input$bins,changes,causing a histogram with 
-#                  a new number of bins to be rendered.", br(),br(),
-#            strong("Hello"), br(),"World")
-      
-      list("This small ",  strong("Shiny application"), "demonstrates Shiny's automatic UI updates.", br(),br(), 
+      list("This small Shiny application aims to help you get a quick glimpse of 
+           your ", strong("product inventory"), ". It demonstrates Shiny's automatic UI updates.", br(),br(), 
            "To start using the app, ", br(), "* decide on your price range by moving the slider or ", br(), 
            "* pick your product type or ", br(), 
            "* choose country of origin from the drop down list", br(),br(),
 
           "Notice how all the related data are automatically re-evaluated and refreshed.",
            br(),br(),
-           "Also click a tab such as ", strong("Plot"), "or ", strong("Tally"), "to see corresponding histogram plot and data display based on your search criteria.")
+           "Also click a tab such as ", strong("Plot"), "or ", strong("Tally"), "to view corresponding histogram 
+          plot and data display change for your search criteria.")
 
     })
 
